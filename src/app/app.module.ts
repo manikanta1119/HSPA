b07e0457011bad73551507475a4deb817335eba8
manikanta1119 <45948056+manikanta1119@users.noreply.github.com>
2020-10-12 23:21:45 -0500

    Components are completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { propertyCardComponent } from './Property/property-card/property-card.component';
+import { PropertyListComponent } from './Property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    propertyCardComponent,
+    PropertyListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
